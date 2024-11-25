20d2a5cdf2354ab6531f54740469c6a23ff165b5
namizz <139370392+namizz@users.noreply.github.com>
2024-11-25 20:22:26 +0300

    database connection sucessfully

--- back-end/db/connect.js
+++ back-end/db/connect.js
@@ -1,12 +1,16 @@
-const Pool = require("pg").Pool;
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "aau_4k_fellowship_members",
-  password: "1234nnamii1234",
-  port: "5432",
+  user: "arat_kilo_fellowship_user",
+  host: "ct2afb52ng1s73ecsvf0-a.oregon-postgres.render.com", // Use the correct host provided by Render
+  database: "arat_kilo_fellowship",
+  password: "SZMSfvGxUW9vm2FkWktH21PN2PMg5iJU",
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false, // Disable certificate validation (only for development)
+  },
 });
+
 pool.connect((err, client, release) => {
   if (err) {
     console.error("Error connecting to the database", err.stack);
@@ -15,4 +19,5 @@ pool.connect((err, client, release) => {
     release();
   }
 });
+
 module.exports = pool;
