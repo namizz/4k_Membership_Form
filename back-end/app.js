60200e604a05dc6daf25c00f95ef14c797c31483
namizz <139370392+namizz@users.noreply.github.com>
2024-11-25 20:28:11 +0300

    port

--- back-end/app.js
+++ back-end/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const app = express();
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
